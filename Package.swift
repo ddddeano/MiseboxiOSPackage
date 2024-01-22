d456dc17076669cb498edcd2b467600a2dc52565
Daniel Watson <d.m.watson1985@gmail.com>
2024-01-22 15:38:42 +0000

    ios 13

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MiseboxiOSPackage",
+    platforms: [
+            .iOS(.v13)  // Set the minimum deployment target to iOS 13
+        ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
