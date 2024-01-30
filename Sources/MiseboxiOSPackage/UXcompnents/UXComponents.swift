7bc27fece12642ee0e1fb3fdef27e0176c748f2c
Daniel Watson Machine 2 <d.m.watson1985gmail.com>
2024-01-30 12:52:09 +0100

    public let userPalette = Palette(color1: MBColourDarkBlue, color2: MBColourPaleGreen)

--- Sources/MiseboxiOSPackage/UXcompnents/UXComponents.swift
+++ Sources/MiseboxiOSPackage/UXcompnents/UXComponents.swift
@@ -242,14 +242,14 @@ public enum TimeType {
     case start, end
 }
 
-let MBColourDarkBlue = Color(red: 0.10, green: 0.20, blue: 0.45) // A deep, dark blue
-let MBColourPaleBlue = Color(red: 0.70, green: 0.85, blue: 0.95) // A light, airy blue
-let MBColourPaleGreen = Color(red: 0.76, green: 0.88, blue: 0.78) // A soft, pale green
-let MBColourDarkGreen = Color(red: 0.13, green: 0.55, blue: 0.13) // A rich, natural green
-let MBColourNaturalPurple = Color(red: 0.58, green: 0.44, blue: 0.86) // A gentle, natural purple inspired by seashells
-let MBColourCream = Color(red: 0.92, green: 0.91, blue: 0.88) // A soft, light cream color
+public let MBColourDarkBlue = Color(red: 0.10, green: 0.20, blue: 0.45) // A deep, dark blue
+public let MBColourPaleBlue = Color(red: 0.70, green: 0.85, blue: 0.95) // A light, airy blue
+public let MBColourPaleGreen = Color(red: 0.76, green: 0.88, blue: 0.78) // A soft, pale green
+public let MBColourDarkGreen = Color(red: 0.13, green: 0.55, blue: 0.13) // A rich, natural green
+public let MBColourNaturalPurple = Color(red: 0.58, green: 0.44, blue: 0.86) // A gentle, natural purple inspired by seashells
+public let MBColourCream = Color(red: 0.92, green: 0.91, blue: 0.88) // A soft, light cream color
 
-struct Palette {
+public struct Palette {
     let color1: Color
     let color2: Color
 
@@ -258,9 +258,8 @@ struct Palette {
         self.color2 = color2
     }
 }
-let userPalette = Palette(color1: MBColourDarkBlue, color2: MBColourPaleGreen)
-let chefPalette = Palette(color1: MBColourDarkBlue, color2: MBColourPaleGreen)
-
+public let userPalette = Palette(color1: MBColourDarkBlue, color2: MBColourPaleGreen)
+public let chefPalette = Palette(color1: MBColourDarkBlue, color2: MBColourPaleGreen)
 
 public struct AvatarView: View {
     var imageUrl: String
