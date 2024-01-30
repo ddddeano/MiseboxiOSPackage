116bf16c789afcb47a4401fc16df7bf0beac0714
Daniel Watson Machine 2 <d.m.watson1985gmail.com>
2024-01-30 12:26:10 +0100

    public var notificationRing: some View {

--- Sources/MiseboxiOSPackage/UXcompnents/UXComponents.swift
+++ Sources/MiseboxiOSPackage/UXcompnents/UXComponents.swift
@@ -260,14 +260,15 @@ public struct AvatarView: View {
             if hasNewContent {
                 Circle()
                     .stroke(LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing),
-                            lineWidth: width * 0.05)
+                            lineWidth: 2) // Reduce the lineWidth to 2
             } else {
                 Circle()
                     .stroke(Color.black, lineWidth: 1)
             }
         }
-        .frame(width: width + (hasNewContent ? width * 0.05 : 1) * 2, height: height + (hasNewContent ? height * 0.05 : 1) * 2)
+        .frame(width: width, height: height) // Remove the extra width and height adjustments
     }
+
 
     public var asyncImage: some View {
         AsyncImage(url: URL(string: imageUrl)) { phase in
